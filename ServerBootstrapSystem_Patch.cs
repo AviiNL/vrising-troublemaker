38b61f13d06d76c61fb15289b3f980033807e8e8
AviiNL <me@avii.nl>
2022-07-04 09:38:05 +0200

    Bugfix: Messages could not be sent to new players on connect.

--- ServerBootstrapSystem_Patch.cs
+++ ServerBootstrapSystem_Patch.cs
@@ -2,22 +2,39 @@ using HarmonyLib;
 using ProjectM;
 using ProjectM.Network;
 using Stunlock.Network;
+using Unity.Entities;
 
 namespace troublemaker;
 
-[HarmonyPatch(typeof(ServerBootstrapSystem), nameof(ServerBootstrapSystem.OnUserConnected))]
-public class ServerBootstrapSystem_Patch
+[HarmonyPatch]
+public static class InitializePlayer
 {
-    public static void Postfix(ServerBootstrapSystem __instance, NetConnectionId netConnectionId)
+    [HarmonyPatch(typeof(ServerBootstrapSystem), nameof(ServerBootstrapSystem.OnUserConnected))]
+    [HarmonyPostfix]
+    public static void OnUserConnected_Patch(ServerBootstrapSystem __instance, NetConnectionId netConnectionId)
     {
-        var em = Plugin.ServerWorld.EntityManager;
         var userIndex = __instance._NetEndPointToApprovedUserIndex[netConnectionId];
         var serverClient = __instance._ApprovedUsersLookup[userIndex];
         var userEntity = serverClient.UserEntity;
-        var userComponent = em.GetComponentData<User>(userEntity);
 
-        var name = string.IsNullOrEmpty(userComponent.CharacterName.ToString()) ? "Vampire" : userComponent.CharacterName;
+        var user = __instance.EntityManager.GetComponentData<User>(userEntity);
 
-        Messaging.getInstance().SendMessage(userComponent, ServerChatMessageType.Lore, $"<color=\"white\">Welcome</color> <color=\"red\">{name}</color>, <color=\"yellow\">Troublemaker</color> <color=\"white\">is</color> <color=\"red\">enabled</color> <color=\"white\">on this server!</color>");
+
+        if (user.CharacterName.IsEmpty)
+        {
+            // no name yet, we'll send in TryIsNameValid
+            return;
+        }
+
+        Messaging.getInstance().SendMessage(user, ServerChatMessageType.Lore, $"<color=\"white\">Welcome</color> <color=\"red\">{user.CharacterName}</color>, <color=\"yellow\">Troublemaker</color> <color=\"white\">is</color> <color=\"red\">enabled</color> <color=\"white\">on this server!</color>");
+    }
+
+    [HarmonyPatch(typeof(HandleCreateCharacterEventSystem), nameof(HandleCreateCharacterEventSystem.TryIsNameValid))]
+    [HarmonyPostfix]
+    public static void TryIsNameValid_Patch(bool __result, HandleCreateCharacterEventSystem __instance, Entity userEntity, string characterNameString)
+    {
+        if (!__result) return;
+
+        Messaging.getInstance().SendMessage(userEntity, ServerChatMessageType.Lore, $"<color=\"white\">Welcome</color> <color=\"red\">{characterNameString}</color>, <color=\"yellow\">Troublemaker</color> <color=\"white\">is</color> <color=\"red\">enabled</color> <color=\"white\">on this server!</color>");
     }
 }
